1da59cae47727eed970d6227b9ba96bca144ef43
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-11-06 18:47:03 +0100

    Initial commit

---
+++ src/Auto.java
@@ -0,0 +1,17 @@
+public class Auto {
+    public String znacka;
+    public int vykon;
+    public int aktRychlost;
+
+    void pridaj() {
+        aktRychlost++;
+    }
+
+    void brzdi() {
+        aktRychlost--;
+    }
+
+    void zobraz() {
+        Auto auto = new Auto();
+    }
+}
